8979c5a82bf4c02526035b2f57ae652a44867c39
unknown <aquinasmoriasi@gmail.com>
2022-10-04 00:01:33 +0300

    add the teacher class

--- person.rb
+++ person.rb
@@ -1,21 +1,21 @@
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
-    def initialize(age, name = "Unkown", parent_permission = true)
-        @id = Random.rand(1..100000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  attr_reader :id
+  attr_accessor :name, :age
 
-    private def  is_of_age?
-        @age > 18
-    end
+  def initialize(age, name: 'Unkown', parent_permission: true)
+    @id = Random.rand(1..100_000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-        is_of_age? || @parent_permission
-    end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age > 18
+  end
 end
-
-
-
--- student.rb
+++ student.rb
@@ -1,14 +1,12 @@
-require_relative './person.rb'
+require_relative './person'
 
 class Student < Person
-    def initialize(classroom)
-        super("unkown")
-        @classroom = classroom
-    end
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
-end
+  def initialize(classroom)
+    super(age)
+    @classroom = classroom
+  end
 
-x=Student.new("History")
-puts(x.play_hooky)
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
