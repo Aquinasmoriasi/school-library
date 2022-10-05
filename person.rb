38180592da649950e117ba7165f7d8d0f6f4ff61
unknown <aquinasmoriasi@gmail.com>
2022-10-05 13:06:44 +0300

    add method to be decorated

--- person.rb
+++ person.rb
@@ -1,8 +1,12 @@
-class Person
+require_relative './nameable.rb'
+require_relative './trimmerdecorator.rb'
+require_relative './capitalizedecorator.rb'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name: 'Unkown', parent_permission: true)
+  def initialize(age, name, parent_permission: true)
     @id = Random.rand(1..100_000)
     @name = name
     @age = age
@@ -13,9 +17,20 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
-    @age > 18
+    @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+p  capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+p  capitalizedTrimmedPerson.correct_name
