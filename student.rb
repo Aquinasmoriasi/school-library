f85b17223108033d077a1d7f291e87ed995edcd5
unknown <aquinasmoriasi@gmail.com>
2022-10-05 21:54:25 +0300

    modify student class to allow setting of classroom

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require_relative './person'
 
 class Student < Person
+  attr_reader :classroom
   def initialize(classroom)
     super(age)
     @classroom = classroom
@@ -8,5 +9,10 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
