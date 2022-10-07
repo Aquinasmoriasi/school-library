5ff358d29b4fd2384a6c7e7f07e73041ca926e1b
unknown <aquinasmoriasi@gmail.com>
2022-10-07 21:07:52 +0300

    modify person.rb file to remove unessesary attributes and methods

--- person.rb
+++ person.rb
@@ -2,35 +2,27 @@ require_relative './nameable'
 require_relative './trimmerdecorator'
 require_relative './capitalizedecorator'
 
-class Person < Nameable
+class Person
   attr_reader :id, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name, parent_permission: true)
-    super()
+  def initialize(age, name)
     @id = Random.rand(1..100_000)
     @name = name
     @age = age
-    @parent_permission = parent_permission
     @rentals = []
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
   end
 
   def correct_name
     @name
   end
 
+  def can_use_services?
+    true
+  end
+
   def add_rental(rental)
     @rentals << rental unless @rentals.include?(rental)
     rental.person = self
-  end
-
-  private
-
-  def of_age?
-    @age >= 18
   end
 end
